45a0b0b12bcc9fe9eb22671317e1468517639ac3
panhaiming <2574958437@qq.com>
2018-04-24 20:34:01 +0800

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -12,8 +12,8 @@ def xuanzhuan(img):
   heightNew=int(width*fabs(sin(radians(degree)))+height*fabs(cos(radians(degree))))
   widthNew=int(height*fabs(sin(radians(degree)))+width*fabs(cos(radians(degree))))
   matRotation=cv.getRotationMatrix2D((width/2,height/2),degree,1)
-  matRotation[0,2] +=(widthNew-width)/2  #重点在这步，目前不懂为什么加这步
-  matRotation[1,2] +=(heightNew-height)/2  #重点在这步
+  matRotation[0,2] +=(widthNew-width)/2  
+  matRotation[1,2] +=(heightNew-height)/2  
   imgRotation=cv.warpAffine(img,matRotation,(widthNew,heightNew),borderValue=(255,255,255))
   return imgRotation
 def distance_n(id):
@@ -23,11 +23,9 @@ def distance_n(id):
     player = cv.matchTemplate(img, player_template, cv.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(player)
     player_spot = (max_loc[0] + 25, max_loc[1] + 150)
-    # 绘制player位置标记
-    img_blur = cv.GaussianBlur(img, (5, 5), 0)  # 高斯模糊
-    canny_img = cv.Canny(img_blur, 1, 10)  # 边缘检测
+    img_blur = cv.GaussianBlur(img, (5, 5), 0)  
+    canny_img = cv.Canny(img_blur, 1, 10)  
     height, width = canny_img.shape
-    # 消除多余的头部
     for y in range(max_loc[1]-5, max_loc[1] + 150):
         for x in range(max_loc[0]-5, max_loc[0] + 50):
             canny_img[y][x] = 0
@@ -39,7 +37,7 @@ def distance_n(id):
         crop_img1 = img[300:max_loc[1]+42 , max_loc[0]+25 :width, :]  # 裁切
     crop_h, crop_w = crop_img.shape
     point_mid = np.zeros((crop_h, crop_w, 1), dtype=np.uint8)
-    center_x, center_y = 0, 0  # 临时变量
+    center_x, center_y = 0, 0 
     max_x = 0
     num_point = 0
     w_point=0
@@ -51,7 +49,6 @@ def distance_n(id):
     if num_point>260 and num_point<320:
        crop_img=point_mid
        w_point=1
-    # 计算中央点
     for y in range(crop_h):
         for x in range(crop_w):
             if crop_img[y, x] == 255:
@@ -80,14 +77,11 @@ def distance_n(id):
                         center_y = y
                         max_x = x
     if max_loc[0] + 25 > int(width / 2):
-        block_center = (center_x + 20, center_y + 300)  # 中央点
+        block_center = (center_x + 20, center_y + 300)  
     else:
         block_center = (center_x + max_loc[0]+25 , center_y + 300)  # 中央点
-    #print("玩家位置", player_spot)
-    #print("落点位置", block_center)
     distance1 = (block_center[0] - player_spot[0]) * (block_center[0] - player_spot[0]) + (block_center[1] - player_spot[1]) * (block_center[1] - player_spot[1])
     distance1 = sqrt(distance1)
-    #cv.line(img, player_spot, block_center, (0, 255, 0), 5)  # 绘制直线
     filename=str(id)+'.png'
     cv.imwrite(filename,img)
     return distance1
